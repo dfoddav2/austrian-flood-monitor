58f930fe113909e623a7d24a04bbd336fe3bf081
dfoddav2 <david.fodor.work@gmail.com>
2024-11-04 15:20:22 +0100

    feat: added Next.js frontend in `client` folder

---
+++ client/src/app/testing/page.tsx
@@ -0,0 +1,11 @@
+export default function Testing() {
+  return (
+    <div>
+      <h1>This is a page for testing purposes</h1>
+      <p>
+        Here we show that we can make requests to the server and fetch from the
+        DB.
+      </p>
+    </div>
+  );
+}

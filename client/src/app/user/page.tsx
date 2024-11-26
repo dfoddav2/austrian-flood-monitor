a17165cf47fd1b6102b379ba5e169300f89ddc62
dfoddav2 <david.fodor.work@gmail.com>
2024-11-26 10:01:49 +0100

    chore: added `/user` page and changing `NavBar` based on `authStore`

---
+++ client/src/app/user/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useAuthStore } from "@/store/authStore";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+// import Link from "next/link";
+// import styles from "./page.module.css";
+
+export default function UserPage() {
+  const user = useAuthStore((state) => state.user);
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">User page</h1>
+      {user && <p>Welcome, {user.username}!</p>}
+      {!user && (
+        <>
+          <p>You are not logged in.</p>
+          <Link href="/login" passHref>
+            <Button>Login</Button>
+          </Link>
+        </>
+      )}
+    </div>
+  );
+}
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -13,13 +13,14 @@ import { ModeToggle } from "@/components/ui/theme-toggle";
 
 export default function Navbar() {
   const user = useAuthStore((state) => state.user);
+  const handleLogout = useAuthStore((state) => state.clearToken);
 
   return (
     <nav className="bg-gray-800 dark:bg-gray-900 p-4 w-full flex justify-between items-center">
       <Link href="/" className="text-white text-2xl font-bold">
         AFM
       </Link>
-      <div className="flex gap-1">
+      <div className="flex gap-2 items-center">
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
@@ -29,18 +30,38 @@ export default function Navbar() {
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/login" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Login
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
+            {!user && (
+              <NavigationMenuItem>
+                <Link href="/login" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Login
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            )}
+            {user && (
+              <>
+                <NavigationMenuItem className="cursor-pointer">
+                  <NavigationMenuLink
+                    className={navigationMenuTriggerStyle()}
+                    onClick={handleLogout}
+                  >
+                    Log out
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <Link href="/user" legacyBehavior passHref>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      {user.username}
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              </>
+            )}
           </NavigationMenuList>
         </NavigationMenu>
-        {user && (
-          <span className="text-white ml-4">Welcome, {user.username}</span>
-        )}
         <ModeToggle />
       </div>
     </nav>

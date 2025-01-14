26c95f9ea887c4cb27958c34656d5b7c8ea15fb2
morrozevich <150383856+morrozevich@users.noreply.github.com>
2025-01-14 19:12:13 +0100

    added historic data visualisation for rivers to a map

---
+++ client/src/components/data/riverData.ts
@@ -0,0 +1,82 @@
+export const riverData = [
+  {
+    id: 1,
+    name: "Danube - Vienna",
+    lat: 48.2082,
+    lng: 16.3719,
+    waterLevel: 3.2,
+    description: "Major monitoring point at Vienna"
+  },
+  {
+    id: 2,
+    name: "Inn River - Innsbruck",
+    lat: 47.2672,
+    lng: 11.4194,
+    waterLevel: 1.8,
+    description: "Alpine river with seasonal variations"
+  },
+  {
+    id: 3,
+    name: "Mur - Graz",
+    lat: 47.0700,
+    lng: 15.4397,
+    waterLevel: 2.1,
+    description: "Important river in Styria"
+  },
+  {
+    id: 4,
+    name: "Salzach - Salzburg",
+    lat: 47.7997,
+    lng: 13.0378,
+    waterLevel: 2.4,
+    description: "Major river through Salzburg"
+  },
+  {
+    id: 5,
+    name: "Danube - Krems",
+    lat: 48.4093,
+    lng: 15.6002,
+    waterLevel: 3.5,
+    description: "Wachau Valley monitoring station"
+  },
+  {
+    id: 6,
+    name: "Enns - Steyr",
+    lat: 48.0500,
+    lng: 14.4167,
+    waterLevel: 1.9,
+    description: "Junction with Danube"
+  },
+  {
+    id: 7,
+    name: "Drau - Villach",
+    lat: 46.6111,
+    lng: 13.8558,
+    waterLevel: 2.2,
+    description: "Major river in Carinthia"
+  },
+  {
+    id: 8,
+    name: "Traun - Wels",
+    lat: 48.1500,
+    lng: 14.0333,
+    waterLevel: 1.7,
+    description: "Important tributary of the Danube"
+  },
+  {
+    id: 9,
+    name: "March - Hohenau",
+    lat: 48.6000,
+    lng: 16.9000,
+    waterLevel: 2.8,
+    description: "Border river with Slovakia"
+  },
+  {
+    id: 10,
+    name: "Raab - Feldbach",
+    lat: 46.9500,
+    lng: 15.8833,
+    waterLevel: 1.5,
+    description: "Southeastern Austrian river"
+  }
+];
